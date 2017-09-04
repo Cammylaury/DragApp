ffc3eec4a1937254fe057370f80c81b2b1d8aba1
Cameron Laury <CameronL@Camerons-MacBook-Air.local>
2017-09-04 13:24:26 -0500

    initial commit

---
+++ DragNet/MainViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  DragNet
+//
+//  Created by Cameron Laury on 9/3/17.
+//  Copyright © 2017 Cameron Laury. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+    }
+
+
+}
+
